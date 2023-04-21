780be455ecf5f35ec88694c14e79c9aff20b80ea
lh99j <grand2181@gmail.com>
2023-04-21 16:07:57 +0900

    1. 사용자 추가기능을 만들어주세요.

--- src/main/java/kr/ac/jejunu/user/UserDao.java
+++ src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -4,9 +4,7 @@ import java.sql.*;
 
 public class UserDao {
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeju", "root", "Flrnl3570@");
+        Connection connection = getConnection();
 
         PreparedStatement preparedStatement = connection.prepareStatement("select id, name, password from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -24,5 +22,28 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+
+    public void insert(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, password) values (?, ?)", Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        user.setId(resultSet.getLong(1));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/jeju", "root", "Flrnl3570@");
     }
 }
